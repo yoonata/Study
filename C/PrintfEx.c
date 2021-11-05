4697967247565bf178fd0b1f36e4dbae62e303fa
yoonata <37606984+yoonata@users.noreply.github.com>
2021-11-05 20:26:24 +0900

    공부중

---
+++ C/PrintfEx.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int main(void) {
+
+	printf("%d\n", 1234);
+	printf("%d%d\n", 10, 20);
+	printf("%d %d\n", 10, 20);
+	printf("Hello world! \n");
+	printf("My age: %d \n", 20);
+	printf("%d is my point \n", 100);
+	printf("Good \nmorning \neverybody\n");
+
+	printf("\'Hello world!\' \n");
+	printf("\"Hello world!\" \n");
+	printf("Hello world\? \n");
+	printf("Hello world? \n");
+	printf("Hello world\\ \n");
+	printf("Hello world\a \n");
+	printf("백\b스페이스\b \n");
+	printf("Hello world!\r \n");
+	printf("Hello \tworld! \n");
+	printf("Hello \vworld! \n");
+	printf("Hello \fworld! \n");
+	printf("Hello world!\r \n");
+
+	printf("%c \n", 'a');
+	printf("%s \n", "hello world");
+	printf("%d \n", 10);
+	printf("%i \n", 202);
+	printf("%f \n", 123.1234);
+	printf("%f \n", 123.12345);
+	printf("%f \n", 123.123456);
+	printf("%f \n", 123.1234567);
+	printf("%o \n", 164);
+	printf("%o \n", -164);
+	printf("%u \n", -10);
+	printf("%u \n", 10);
+	printf("%x \n", 123);
+	printf("%x \n", -123);
+	printf("%X \n", 123);
+	printf("%X \n", -123);
+	printf("%e \n", 123.1234);
+	printf("%e \n", -123.1234);
+	printf("%E \n", 123.1234);
+	printf("%E \n", -123.1234);
+	printf("%g \n", 123.1234);
+	printf("%g \n", 123.12345);
+	printf("%g \n", 123.123456);
+	printf("%g \n", 123.1234567);
+	printf("%g \n", 0.00001234);
+	printf("%g \n", -123.1234);
+	printf("%G \n", 123.1234);
+	printf("%G \n", 123.12345);
+	printf("%G \n", 123.123456);
+	printf("%G \n", 123.1234567);
+	printf("%G \n", -123.1234);
+	printf("%% \n");
+
+
+	return 0;
+}
